08e1f2c669a9cf0ab3369f84a03378cbc2c03354
Unknown <florian.hourlier@hotmail.fr>
2018-06-14 07:12:27 +0200

    CV : rafistolage
    Remaniement des tailles de fontes, rajout de compétences et restructuration de ces dernières

--- cv/js/card-competence.js
+++ cv/js/card-competence.js
@@ -1,5 +1,5 @@
-const drawCard = (image, title, description, skill) => {
-    document.getElementById("competences").innerHTML += `
+const drawCard = (image, title, description, skill, categorie) => {
+    document.getElementById(categorie).innerHTML += `
     <div class="col-12 col-sm-6 col-md-4 col-lg-3">
     <div class="card">
     <img class="card-img-top" src="./img/${image}">
@@ -12,18 +12,20 @@ const drawCard = (image, title, description, skill) => {
     </div>`
 }
 
-drawCard("html5.png", "HTML5", "Respect des bonnes pratiques W3C", 4)
-drawCard("css3.png", "CSS3", `Exploitation du potentiel des <a href="https://el-loco-pinguino.github.io/fun/pomme.html">animations</a> CSS et de la librairie Bootstrap (mise en &oelig;uvre sur ce CV !)`, 4)
-drawCard("js.png", "JavaScript (client)", `Divers travaux personnels réalisés (<a target="_blank" href="https://el-loco-pinguino.github.io/fun/fight.html">jeu de combat minimaliste</a>, <a target="_blank" href="https://el-loco-pinguino.github.io/fun/excusesBidon.html">générateur d'excuses bidon</a>, ...)`, 4)
-drawCard("node.png", "JavaScript (serveur)", "Utilisation de NodeJS et exploitation des API distantes", 3)
-drawCard("react.png", "React", "Librairie principale et complément de style Reactstrap", 3)
-drawCard("vue.png", "VueJS", "Auto-apprentissage et application lors d'un projet client", 3)
-drawCard("algo.jpg", "Techniques de programmation", "Méthodologie de programmation fonctionnelle", 4)
-drawCard("sql.png", "Bases de données", "Langage SQL intermédiaire, moteurs MySQL et SQLite", 4)
-drawCard("php.jpg", "PHP", "Langage de niveau basique et intermédiaire. Utilisation de PDO pour communiquer avec les bases de données", 3)
-drawCard("wordpress.jpg", "WordPress", "Apte à créer et paramétrer un projet, et à y apporter des modifications d'ordre stylistique", 2)
-drawCard("unionjack.jpg", "Anglais à caractère professionnel", "Niveau estimé à B2/C1 sur le référentiel de niveau européen", 4)
-drawCard("spain.jpg", "Espagnol", "Niveau Terminale. Je souhaiterais réapprendre la langue, pour pouvoir en faire un atout", 3)
-drawCard("office.png", "Microsoft Office", "Version 2007 principalement. Bon niveau sur le trio Word/Excel/PowerPoint", 4)
-drawCard("machineaecrire.png", "Rédaction", "Aisance en orthographe. Rédaction de documents professionnels", 5)
-drawCard("project.jpg", "Méthodologie de projet", "Attribution des tâches et compte-rendu des réalisations", 4)
+drawCard("html5.png", "HTML5", "Respect des bonnes pratiques W3C", 4, "competencesWeb")
+drawCard("css3.png", "CSS3", `Exploitation du potentiel des <a href="https://el-loco-pinguino.github.io/fun/pomme.html">animations</a> CSS et de la librairie Bootstrap (mise en &oelig;uvre sur ce CV !)`, 4, "competencesWeb")
+drawCard("js.png", "JavaScript (client)", `Divers travaux personnels réalisés (<a target="_blank" href="https://el-loco-pinguino.github.io/fun/fight.html">jeu de combat minimaliste</a>, <a target="_blank" href="https://el-loco-pinguino.github.io/fun/excusesBidon.html">générateur d'excuses bidon</a>, ...)`, 4, "competencesWeb")
+drawCard("node.png", "JavaScript (serveur)", "Utilisation de NodeJS et exploitation des API distantes", 3, "competencesWeb")
+drawCard("react.png", "React", "Librairie principale et complément de style Reactstrap", 3, "competencesWeb")
+drawCard("vue.png", "VueJS", "Auto-apprentissage et application lors d'un projet client", 3, "competencesWeb")
+drawCard("algo.jpg", "Techniques de programmation", "Méthodologie de programmation fonctionnelle", 4, "competencesWeb")
+drawCard("sql.png", "Bases de données", "Langage SQL intermédiaire, moteurs MySQL et SQLite", 4, "competencesWeb")
+drawCard("php.jpg", "PHP", "Langage de niveau basique et intermédiaire. Utilisation de PDO pour communiquer avec les bases de données", 3, "competencesWeb")
+drawCard("wordpress.jpg", "WordPress", "Apte à créer et paramétrer un projet, et à y apporter des modifications d'ordre stylistique", 2, "competencesWeb")
+drawCard("unionjack.jpg", "Anglais à caractère professionnel", "Niveau estimé à B2/C1 sur le référentiel de niveau européen", 4, "competencesTrans")
+drawCard("spain.jpg", "Espagnol", "Niveau Terminale. Je souhaiterais réapprendre la langue, pour pouvoir en faire un atout", 3, "competencesDiv")
+drawCard("office.png", "Microsoft Office", "Version 2007 principalement. Bon niveau sur le trio Word/Excel/PowerPoint", 4, "competencesTrans")
+drawCard("machineaecrire.png", "Rédaction", "Aisance en orthographe. Rédaction de documents professionnels", 5, "competencesTrans")
+drawCard("project.jpg", "Méthodologie de projet", "Attribution des tâches et compte-rendu des réalisations", 4, "competencesTrans")
+drawCard("blender.png", "Blender 3D", "Maîtrise des bases du logiciel grâce à des tutoriels vidéo. Apte à faire des objets simples", 2, "competencesDiv")
+drawCard("rm.jpg", "RPG Maker", "Le principal jeu sur lequel je travaille représente bien ma personnalité: un peu absurde", 3, "competencesDiv")
