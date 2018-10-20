a2f35d80dd70ce0d0930a6b08e1cc5df50da0352
Unknown <florian.hourlier@hotmail.fr>
2018-10-20 17:40:25 +0200

    Hackathon - Images de background
    En fonction de la case sur laquelle on est

--- hackathon/js/events.js
+++ hackathon/js/events.js
@@ -10,6 +10,7 @@ const permitTouch = () => {
 
 const showEvent = (xPos, yPos) => {
     if (xPos === 0 && yPos === 0) {
+        updateImage("bg_herbe.png")
         if (!items.seeds) {
             descriptionElement.innerText = "Un sac de graines. Je vais le prendre au cas où."
             items.seeds = true
@@ -18,21 +19,27 @@ const showEvent = (xPos, yPos) => {
         }
     }
     if (xPos === 0 && yPos === 1) {
+        updateImage("bg_herbe.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 0 && yPos === 2) {
+        updateImage("bg_herbe.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 0 && yPos === 3) {
+        updateImage("bg_herbe.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 0 && yPos === 4) {
+        updateImage("bg_herbe.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 0 && yPos === 5) {
+        updateImage("bg_herbe.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 0 && yPos === 6) {
+        updateImage("bg_desert.png")
         if (!items.money) {
             descriptionElement.innerText = "Un gobelin me barre le passage. Il dit qu'il faut payer un péage pour passer par ici."
             lockTouch()
@@ -42,177 +49,235 @@ const showEvent = (xPos, yPos) => {
         }
     }
     if (xPos === 0 && yPos === 7) {
+        updateImage("bg_desert.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 1 && yPos === 0) {
+        updateImage("bg_herbe.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 1 && yPos === 1) {
+        updateImage("bg_herbe.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 1 && yPos === 2) {
+        updateImage("bg_herbe.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 1 && yPos === 3) {
+        updateImage("bg_herbe.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 1 && yPos === 4) {
+        updateImage("bg_herbe.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 1 && yPos === 5) {
+        updateImage("bg_herbe.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 1 && yPos === 6) {
+        updateImage("bg_herbe.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 1 && yPos === 7) {
+        updateImage("bg_desert.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 1 && yPos === 11) {
+        updateImage("bg_neige.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 1 && yPos === 12) {
+        updateImage("bg_neige.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 2 && yPos === 0) {
+        updateImage("bg_herbe.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 2 && yPos === 1) {
+        updateImage("bg_herbe.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 2 && yPos === 2) {
+        updateImage("bg_herbe.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 2 && yPos === 3) {
+        updateImage("bg_herbe.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 2 && yPos === 4) {
+        updateImage("bg_herbe.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 2 && yPos === 5) {
+        updateImage("bg_herbe.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 2 && yPos === 6) {
+        updateImage("bg_herbe.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 2 && yPos === 7) {
+        updateImage("bg_desert.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 2 && yPos === 8) {
+        updateImage("bg_desert.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 2 && yPos === 9) {
+        updateImage("bg_desert.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 2 && yPos === 11) {
+        updateImage("bg_neige.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 2 && yPos === 12) {
+        updateImage("bg_neige.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 3 && yPos === 0) {
+        updateImage("bg_herbe.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 3 && yPos === 1) {
+        updateImage("bg_herbe.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 3 && yPos === 2) {
+        updateImage("bg_herbe.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 3 && yPos === 3) {
+        updateImage("bg_herbe.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 3 && yPos === 4) {
+        updateImage("bg_herbe.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 3 && yPos === 5) {
+        updateImage("bg_herbe.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 3 && yPos === 6) {
+        updateImage("bg_herbe.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 3 && yPos === 7) {
+        updateImage("bg_herbe.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 3 && yPos === 8) {
+        updateImage("bg_herbe.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 3 && yPos === 9) {
+        updateImage("bg_desert.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 3 && yPos === 10) {
+        updateImage("bg_neige.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 3 && yPos === 11) {
+        updateImage("bg_neige.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 3 && yPos === 12) {
+        updateImage("bg_neige.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 4 && yPos === 2) {
+        updateImage("bg_herbe.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 4 && yPos === 3) {
+        updateImage("bg_herbe.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 4 && yPos === 4) {
+        updateImage("bg_herbe.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 4 && yPos === 5) {
+        updateImage("bg_herbe.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 4 && yPos === 6) {
+        updateImage("bg_herbe.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 4 && yPos === 7) {
+        updateImage("bg_desert.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 4 && yPos === 8) {
+        updateImage("bg_desert.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 4 && yPos === 9) {
+        updateImage("bg_desert.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 4 && yPos === 10) {
+        updateImage("bg_neige.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 4 && yPos === 11) {
+        updateImage("bg_neige.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 4 && yPos === 12) {
+        updateImage("bg_neige.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 5 && yPos === 2) {
+        updateImage("bg_herbe.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 5 && yPos === 3) {
+        updateImage("bg_herbe.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 5 && yPos === 4) {
+        updateImage("bg_herbe.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 5 && yPos === 5) {
+        updateImage("bg_herbe.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 5 && yPos === 6) {
+        updateImage("bg_herbe.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 5 && yPos === 7) {
+        updateImage("bg_herbe.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 5 && yPos === 8) {
+        updateImage("bg_herbe.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 5 && yPos === 9) {
+        updateImage("bg_herbe.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 5 && yPos === 10) {
+        updateImage("bg_neige.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 5 && yPos === 11) {
+        updateImage("bg_neige.png")
         descriptionElement.innerText = "Crayon !"
     }
     if (xPos === 5 && yPos === 12) {
+        updateImage("bg_neige.png")
         descriptionElement.innerText = "Crayon !"
     }
 }
--- hackathon/js/swipe.js
+++ hackathon/js/swipe.js
@@ -9,25 +9,21 @@ const hammertime = new Hammer(bottomScreenElement)
 hammertime.get('swipe').set({ direction: Hammer.DIRECTION_ALL })
 if (!gameOver) {
     hammertime.on('swipeup', function(ev) {
-        updateImage("cerise.png")
         caseValue = map[xCurr][yCurr]
         moveNorth(xCurr, yCurr)
         console.log("Position actuelle: " + xCurr + ", " + yCurr)
     })
     hammertime.on('swipeleft', function(ev) {
-        updateImage("fraise.png")
         caseValue = map[xCurr][yCurr]
         moveWest(xCurr, yCurr)
         console.log("Position actuelle: " + xCurr + ", " + yCurr)
     })
     hammertime.on('swipedown', function(ev) {
-        updateImage("melon.png")
         caseValue = map[xCurr][yCurr]
         moveSouth(xCurr, yCurr)
         console.log("Position actuelle: " + xCurr + ", " + yCurr)
     })
     hammertime.on('swiperight', function(ev) {
-        updateImage("pomme.png")
         caseValue = map[xCurr][yCurr]
         moveEast(xCurr, yCurr)
         console.log("Position actuelle: " + xCurr + ", " + yCurr)
