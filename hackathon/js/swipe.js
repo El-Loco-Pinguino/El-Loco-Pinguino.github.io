0ef906beb4047f91392c4ef9d1d577efb7e7b8a7
Unknown <florian.hourlier@hotmail.fr>
2018-10-20 08:59:50 +0200

    Hackathon - Swipe et action
    Change l'image affichée sur l'écran supérieur

--- hackathon/js/swipe.js
+++ hackathon/js/swipe.js
@@ -1,15 +1,24 @@
 const bottomScreenElement = document.getElementById("touch")
-var hammertime = new Hammer(bottomScreenElement);
+
+const updateImage = image => {
+    document.getElementById("action").style.backgroundImage = `url("./res/${image}")`
+}
+
+const hammertime = new Hammer(bottomScreenElement)
 hammertime.get('swipe').set({ direction: Hammer.DIRECTION_ALL })
 hammertime.on('swipeup', function(ev) {
-	alert("Swipe en haut")
+    alert("Swipe en haut")
+    updateImage("cerise.png")
 })
 hammertime.on('swipeleft', function(ev) {
-	alert("Swipe à gauche")
+    alert("Swipe à gauche")
+    updateImage("fraise.png")
 })
 hammertime.on('swipedown', function(ev) {
-	alert("Swipe en bas")
+    alert("Swipe en bas")
+    updateImage("melon.png")
 })
 hammertime.on('swiperight', function(ev) {
-	alert("Swipe à droite")
+    alert("Swipe à droite")
+    updateImage("pomme.png")
 })
